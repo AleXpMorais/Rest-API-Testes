e30625d35f4622e1f401d718460723a866d6dabb
Alexandre Morais <piekatoski2001@gmail.com>
2021-08-18 12:18:05 -0300

    teste inicial

---
+++ src/main/java/expertostech/tutorial/rest/api/TutoriaRestApiApplication.java
@@ -0,0 +1,13 @@
+package expertostech.tutorial.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoriaRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutoriaRestApiApplication.class, args);
+    }
+
+}
