756c157425b70febe227648a007454cd0ff08438
Alexandre Morais <piekatoski2001@gmail.com>
2021-08-18 12:21:49 -0300

    teste inicial

---
+++ src/test/java/expertostech/tutorial/rest/api/TutoriaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package expertostech.tutorial.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoriaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
